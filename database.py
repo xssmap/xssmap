420ad3ab9cb7d88ce45a5483837820abe3a77cbe
xssmap <3065886491@qq.com>
2019-08-14 22:50:42 +0800

    db

--- database.py
+++ database.py
@@ -1,3 +1,15 @@
 import sqlite3
 
 
+class Database:
+    def __init__(self):
+        self.rows = []
+        self.connection = sqlite3.connect('xssmap.db')
+        self.cursor = self.connection.cursor().execute('select payload from xssmap')
+        for row in self.cursor:
+            self.rows += row
+
+    def __del__(self):
+        self.connection.close()
+
+database = Database()
