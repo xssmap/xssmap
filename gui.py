4e51aa5184aace2b91c19d42dfeeb5130ed3fb92
xssmap <3065886491@qq.com>
2019-08-18 18:40:27 +0800

    gui

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 import tkinter
 from tkinter import ttk
+from tkinter import messagebox
+
 
 class Gui:
     def __init__(self):
@@ -10,11 +12,30 @@ class Gui:
         self.width = 1024
         self.window.geometry("%dx%d+%d+%d" % (self.width, self.height, (self.window.winfo_screenwidth() - self.width) / 2, (self.window.winfo_screenheight() - self.height) / 2))
         self.window.resizable(width=False, height=False)
+        self.text1 = None
+        self.button1 = None
+        self.button2 = None
         self.setframe1()
         self.setframe2()
         self.setframe3()
         self.setframe4()
         self.window.mainloop()
+
+    def setflag(self):
+        try:
+            self.text1.tag_configure('red', foreground='#DC143C')
+            self.text1.insert(tkinter.SEL_FIRST, "$", 'red')
+            self.text1.insert(tkinter.SEL_LAST, "$", 'red')
+            self.button1['state'] = tkinter.DISABLED
+        except tkinter.TclError:
+            messagebox.showerror("提示", "请选中标记参数")
+
+    def unsetflag(self):
+        content = self.text1.get("0.0", "end").strip('\n')
+        self.text1.delete("0.0", "end")
+        self.text1.insert("0.0", content.replace('$', ''))
+        self.button1['state'] = tkinter.NORMAL
+        self.text1.see(1000.0)
 
     def setframe1(self):
         frame = tkinter.Frame(self.window, height=350)
@@ -22,8 +43,8 @@ class Gui:
         frame.pack_propagate(0)
         label = tkinter.Label(frame, text="XSS输入点")
         label.pack()
-        text = tkinter.Text(frame)
-        text.pack(fill=tkinter.X)
+        self.text1 = tkinter.Text(frame)
+        self.text1.pack(fill=tkinter.X)
 
     def setframe2(self):
         frame = tkinter.Frame(self.window, height=50)
@@ -33,10 +54,10 @@ class Gui:
         frame1.pack(side=tkinter.LEFT)
         frame2 = tkinter.Frame(frame)
         frame2.pack(side=tkinter.RIGHT)
-        button = tkinter.Button(frame1, text="标记参数")
-        button.pack(ipadx=200)
-        button2 = tkinter.Button(frame2, text="清除标记")
-        button2.pack(ipadx=200)
+        self.button1 = tkinter.Button(frame1, text="标记参数", command=self.setflag)
+        self.button1.pack(ipadx=200)
+        self.button2 = tkinter.Button(frame2, text="清除标记", command=self.unsetflag)
+        self.button2.pack(ipadx=200)
 
     def setframe3(self):
         frame = tkinter.Frame(self.window, height=350)
@@ -59,8 +80,13 @@ class Gui:
         frame2.pack_propagate(0)
         label = tkinter.Label(frame1, text="线程数")
         label.pack(side=tkinter.LEFT, padx=80)
-        combobox = ttk.Combobox(frame1)
+        combobox = ttk.Combobox(frame1, state="readonly")
         combobox.pack(side=tkinter.LEFT)
+        thread = []
+        for i in range(1, 21):
+            thread.append(i)
+        combobox['value'] = thread
+        combobox.current(0)
         button = tkinter.Button(frame2, text="开始")
         button.pack(side=tkinter.LEFT, ipadx=300)
 
