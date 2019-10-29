a1423ee91bb19a19c0116537edaeadc94cc36fd1
xssmap <3065886491@qq.com>
2019-10-29 14:43:09 +0800

    new

--- crawler.py
+++ crawler.py
@@ -1,6 +1,22 @@
 import requests
+import re
+import urllib3
 
 
 class Crawler:
-    def __init__(self):
-        pass
+    def __init__(self, domain):
+        self.domain = domain
+        self.urls = []
+
+    def stepone(self):
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        r = requests.get(url=self.domain, verify=False)
+        content = r.text
+        href = re.findall('href="(.*?)"', content)
+        for url in href:
+            if self.domain in url:
+                print url
+
+
+crawler = Crawler("https://www.qq.com")
+crawler.stepone()
